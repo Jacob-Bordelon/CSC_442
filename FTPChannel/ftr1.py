a06422e186d9ace394752b86675f2550e4716322
EboniWilliams <maiijae.ew@gmail.com>
2019-09-25 11:05:08 -0500

    method 10 works on ftr1.py

--- FTPChannel/ftr1.py
+++ FTPChannel/ftr1.py
@@ -1,8 +1,7 @@
 #python2, linux OS
 import os
-from ftplib import FTP
 #variable to determine whether to do 7 or 10
-METHOD = 7
+METHOD = 7 
 
 # this function converts a binary number
 # into a 7-bit ASCII character
@@ -22,33 +21,29 @@ def getBinary(x):
     return permissions
 ######################################################################
 # fetch the file listing with permission from FTP server
-source = []
-ftp = FTP('jeangourd.com')
-ftp.login()
-ftp.cwd("/"+str(METHOD))
-ftp.retrlines('LIST',source.append)
-#for test purposes
-#os.system("ls -lh >> files.txt")
+os.system("ls -lh >> files.txt")
 
 # start decoding message hidden in permissions by grabbing
 # the list of files, isolating the permissions for each file,
 # and then decoding the permissions
 bin_msg = ""
-for a_file in source:
-    bin_msg += str(getBinary(a_file[0:10]))
+source = open('files.txt','r').readlines() 
+for line in source:
+    attributes = line.split(' ')
+    if "total" not in attributes[0]:
+        bin_msg += str(getBinary(attributes[0]))
+
 # With the binary message, generate the covert message
 # and output to the terminal
 secret = ""
 # check if 7bit or 10bit so can shift accordingly
 shift = 3 if (METHOD == 7) else 0
-for i in range(0, len(bin_msg),10):
+for i in range(0, len(bin_msg)-10,10):
     # skip if any of first 3 bits is set
     if((METHOD == 7) and ("1" in bin_msg[i:i+3])):
-        continue
-    secret += getASCII(bin_msg[i+shift:i+shift+7])
+        continue;
+    secret += getASCII(bin_msg[i+shift:i+10])
 print secret
-# file cleanup
-#os.system("rm files.txt")
-#close server
-ftp.close()
 
+# file cleanup
+os.system("rm files.txt")
