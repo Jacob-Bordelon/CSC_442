6265e8a4d5ebaff72564b6852417ebade6c2783a
Jacob-Bordelon <43796428+Jacob-Bordelon@users.noreply.github.com>
2019-10-12 19:02:24 +0000

    Add files via upload

--- XOR/xor.py
+++ XOR/xor.py
@@ -1,81 +1,66 @@
+
+# File:     xor.py
+# Team:     Johnmichael Book, Jacob Bordelon, Tyler Nelson,
+#           Logan Simmons, Eboni Williams, Breno Yamada Riquieri
+# Version:  10.12.19
+# Usage:    Use python 2.7
+#           Example: python xor.py < ciphertext 
+#                    python xor.py < plaintext > ciphertext
+# Github:   https://github.com/Jacob-Bordelon/CSC_442.git
+# Descr:    This program reads a text file from stdin and a key in the current directory
+#           it will compare the binary value of the text and key, xor'ing each binary bit
+#           them return the xor'd data to stdout
+
+
 import sys
-from binascii import unhexlify
+from binascii import unhexlify, hexlify
 
-# you need to convert the non characters into binary values
-# this is done by taking the decimal value of the each indivual character, converting that to binary (line 10)
-# since we want the string to maintain its length, we extend the length of each bit so it matches 8 bits (line 11)
-# after the character has been converted, combine the binary string into one massive integer array to save 
-# from having to convert later on
-def convertToBits(string):
-    result = []
-    for c in string:
-        bits = bin(ord(c))[2:]
-        bits = '00000000'[len(bits):] + bits
-        result.extend([int(b) for b in bits])
-    return result
+# this function takes one character as input and returns the binary equivalent as a string
+def binary(byte):
+    return "{0:08b}".format(int(hex(ord(byte)),16))
 
-# The xor operation works very simply, when comparing just two bits. since youre only comparing two bits
-# theres only 4 possiblities the conversion can be. either they match or they dont
-# a dictionary of the 4 values saves spaces and looks cleaner than a line of if statements
-# the function takes two bytes (a,b) and searches for it in the dictionary. if the bits match, the dictionary returns
-# a zero. if they dont match, it returns a 1
-def xor(a,b):
-    dictionary={(0,0):0,(1,1):0,(0,1):1,(1,0):1}
-    return dictionary[(a,b)]
+# the toAscii function takes a string of binary bytes and converts it to character strings
+# it then returns the string as a bytearray
+def toAscii(byte):
+    b = ""
+    i=0
+    while i < len(byte):
+        b+=chr(int(byte[i:i+8],2))
+        i+=8
+    return bytearray(b)
 
-# convert the binary bits to ascii characters
-def toASCII(num):
-    msg = ""
-    i = 0
-    while (i < len(num)):
-        # process one byte at a time
-        b = num[i:i+8]
-        # convert to ASCII
-        n = int("0b{}".format(b),2)
-        try:
-            msg += unhexlify("{0:x}".format(n))
-        except TypeError:
-            msg += "?"
-        # stop at "EOF"
-        i += 8
-    return msg
+# the xor program takes two binary strings as the parameters and returns the xor'd value 
+# the strings can be any length
+def xor(t,k):
+    h = ""
+    for i in range(len(t)):
+        h+=str(int(t[i])^int(k[i]))
+    return h
 
-
-
-
-# the key wont be passed as an argument, it will exist in the same directory as the function
-# so, we need to open up the file and read its contents. passing them as the file variable 'key'
-key = open('key', 'r').read()
-
-# the plain/cipher text is to be taken as a paramenter, so we need to read it from standard input
+# text takes either the ciphertext or plaintext as the parameter from stdin
+# key must be a file in the current directory
+# NOTE: use 'cp [name of key] key' to change the key you want to use
 text = sys.stdin.read()
+key = open('key','rb').read()
+
+# convert both files from character files to binary files
+# the ''.join() combines all the values of the list
+# NOTE: if either the key or text file is already a binary file, just comment that like out and write k=key or t = text
+t = ''.join([binary(i) for i in text])
+k = ''.join([binary(i) for i in key])
+
+#xor the two converted and joined binary strings
+x = xor(t,k)
+
+# convert the xor'd binary string to an array
+a = toAscii(x)
+
+# send the converted ascii key to standard output
+sys.stdout.write(a)
 
 
 
-# Once we have our documents, we need to convert the documents to binary strings, making sure they have
-# they same length even after theyve been converted.
-# we lable these as binaryKey and binaryText, or bKey and bText for short
-bText = convertToBits(text)
-bKey = convertToBits(key)
 
 
-
-
-# now iterate through the bKey and bText, xor'ing each binary bit, and adding the output to the result
-# variable
-
-result = ""
-for i in range(len(bKey)):
-    result+=str(xor(bKey[i],bText[i]))
-
-
-
-# lastly, print out the result
-
-# NOTE this will return the binary string, we need to pass it through the binary.py converter
-
-print toASCII(result)
-
-
-
+   
 
