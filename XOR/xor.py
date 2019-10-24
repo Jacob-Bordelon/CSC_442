d455c678b658be943446b54a91fd19b52f8f61d7
EboniWilliams <maiijae.ew@gmail.com>
2019-10-24 13:57:34 -0500

    added a few comments to xor

--- XOR/xor.py
+++ XOR/xor.py
@@ -1,4 +1,3 @@
-
 # File:     xor.py
 # Team:     Johnmichael Book, Jacob Bordelon, Tyler Nelson,
 #           Logan Simmons, Eboni Williams, Breno Yamada Riquieri
@@ -6,20 +5,19 @@
 # Usage:    Use python 2.7
 #           Example: python xor.py < ciphertext 
 #                    python xor.py < plaintext > ciphertext
-# Github:   https://github.com/Jacob-Bordelon/CSC_442.git
+# Github:   https://github.com/Jacob-Bordelon/CSC_442/blob/master/XOR/xor.py
 # Descr:    This program reads a text file from stdin and a key in the current directory
 #           it will compare the binary value of the text and key, xor'ing each binary bit
-#           them return the xor'd data to stdout
-
-
+#           them return the xor'd data to stdout.
+#=================================== IMPORT STATEMENTS ========================================#
 import sys
 from binascii import unhexlify, hexlify
-
-# this function takes one character as input and returns the binary equivalent as a string
+#======================================= METHODS ==============================================#
+# This function takes one character as input and returns the binary equivalent as a string
 def binary(byte):
     return "{0:08b}".format(int(hex(ord(byte)),16))
 
-# the toAscii function takes a string of binary bytes and converts it to character strings
+# This function takes a string of binary bytes and converts it to character strings
 # it then returns the string as a bytearray
 def toAscii(byte):
     b = ""
@@ -29,14 +27,14 @@ def toAscii(byte):
         i+=8
     return bytearray(b)
 
-# the xor program takes two binary strings as the parameters and returns the xor'd value 
+# This function takes two binary strings as the parameters and returns the xor'd value 
 # the strings can be any length
 def xor(t,k):
     h = ""
     for i in range(len(t)):
         h+=str(int(t[i])^int(k[i]))
     return h
-
+#========================================= MAIN PROGRAM =========================================#
 # text takes either the ciphertext or plaintext as the parameter from stdin
 # key must be a file in the current directory
 # NOTE: use 'cp [name of key] key' to change the key you want to use
@@ -57,10 +55,3 @@ a = toAscii(x)
 
 # send the converted ascii key to standard output
 sys.stdout.write(a)
-
-
-
-
-
-   
-
