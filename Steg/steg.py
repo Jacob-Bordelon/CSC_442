f4f68da16e23652420f40c5058b4856a744ebf8f
EboniWilliams <maiijae.ew@gmail.com>
2019-10-24 13:38:54 -0500

    added fileNotFound handling in params

--- Steg/steg.py
+++ Steg/steg.py
@@ -1,29 +1,57 @@
+# File:     steg.py
+# Team:     THE ROMANS->
+#           Jonmichael Book, Jacob Bordelon, Tyler Nelson,
+#           Logan Simmons, Eboni Williams, Breno Yamada Riquieri
+# Version:  10.22.19
+# Usage:    Use python 2.7
+#           Example: python steg.py [--help] [-b] [-B] [-s] [-r] [-o OFFSET] [-i INTERVAL]
+#               [-w WRAPPER_FILE] [-h HIDDEN_FILE]
+#           Note: -b and -B should not be used at the same time and both INTERVAL
+#                 and HIDDEN_FILE are optional paramets
+#           Help: python steg.py --help
+# Github:   https://github.com/Jacob-Bordelon/CSC_442/blob/master/Steg/steg.py
+# Descr:    This program can use either bit method or byte method of steganography
+#           to either store or read hidden files or messages inside of a bitmap file.
+#=================================== IMPORT STATEMENTS ===================================#
 from sys import stdin, stdout
 import argparse
 from math import floor
-
+#======================================= VARIABLES =======================================#
+# Sentinal is used to indicate the end of the message/hidden file
+sentinal = "0x0 0xff 0x0 0x0 0xff 0x0" # change this value if the sentinal changes 
 # Grab terminal parameters as flags
 parser = argparse.ArgumentParser(prog='steg.py',conflict_handler="resolve")
 parser.add_argument("-b","--bit", help="Use bit method",action='store_true')
 parser.add_argument("-B","--byte", help="Use byte method", action="store_true")
 parser.add_argument("-s","--store", help="Store (and hide) data", action="store_true")
 parser.add_argument("-r","--retrieve", help="Retrieve hidden data", action="store_true")
-parser.add_argument("-o","--offset",help="Set offset to <val>",type=int)
+parser.add_argument("-o","--offset",default=0,help="Set offset to <val>",type=int)
 parser.add_argument("-i","--interval",default=1,help=" Set interval to <val>",type=int)
 parser.add_argument("-w","--wrapper_file",help="Set wrapper file to <val>")
 parser.add_argument("-h","--hidden_file",help="Set hidden file to <val>")
 args = parser.parse_args()
-
-
+#======================================== CLASSES ========================================#
 # check and organize parameters 
 class params:
+    # Constructor for params
+    # Given a set of arguments and a sentinal, extract the relevant data and assign to
+    # the correct fields in the class. If one of the files can't be found, the program
+    # will exit.
     def __init__(self,args,sentinal):
         self.bitMethod = '-b' if args.bit == True else '-B'
         self.dataMethod = '-s' if args.store == True else "-r"
         self.interval = args.interval
         self.offset = int(args.offset)
-        self.wrapper = bytearray(open(args.wrapper_file,'rb').read()) if args.wrapper_file != None else bytearray()
-        self.hidden = bytearray(open(args.hidden_file,'rb').read()) if args.hidden_file != None else bytearray()
+        try:
+            self.wrapper = bytearray(open(args.wrapper_file,'rb').read()) if args.wrapper_file != None else bytearray()
+        except IOError:
+            print "Wrapper file: \""+args.wrapper_file+"\"not found."
+            exit()
+        try:
+            self.hidden = bytearray(open(args.hidden_file,'rb').read()) if args.hidden_file != None else bytearray()
+        except IOError:
+            print "Hidden file: \""+args.hidden_file+"\" not found."
+            exit()
         self.sentinal = sentinal.split(' ')
     
     # Calculate the best interval given a wrapper and hidden file
@@ -37,24 +65,27 @@ class params:
     # return the maximum size hidden file a wrapper could hold
     def maxHiddenSize(self):
         return floor((len(self.wrapper) - self.offset - len(self.sentinal))/self.interval)
-
-
-
-# Byte Method
-
-#Extract
+#======================================== CLASSES ========================================#
+#  This method extracts a hidden message or file from a file using the Byte Method
 def byteExtraction(File):
-    o = File.offset
-    I = File.interval
+    o = File.offset # how far down to start looking
+    I = File.interval # iterations to look at
     S = File.sentinal
     H = File.hidden
     W = File.wrapper
 
+    # only continue if the offset is less than the length
+    # of the wrapper file
     while o < len(W):
+        # get the byte at the offset
         b = W[o]
+        # if the byte is not in the the sentinal, added it
+        # to the hidden file
         if b not in S:
             H+=chr(b)
+        # increase the offset
         o+=I
+    # output the hidden file
     stdout.write(H)
     
 
@@ -142,9 +173,6 @@ def bitStorage(File):
     
 
 # ////////////////////[   MAIN   }///////////////////////////////////////
-sentinal = "0x0 0xff 0x0 0x0 0xff 0x0" # change this value if the sentinal changes 
-
-
 if __name__ == "__main__":
     
     a = params(args,sentinal) # first, create a params instance to organize data given from the flags
